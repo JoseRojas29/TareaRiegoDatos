0caeb7fed7608983c4c61fafc1687ab9670651a3
JoseRojas29 <josegabriel291106@gmail.com>
2025-10-14 11:05:37 -0600

    Métodos de Zona de riego

--- ZonaRiego.ino
+++ ZonaRiego.ino
@@ -23,3 +23,50 @@ public:
 };
 
 #endif
+
+// ZonaRiego.cpp
+#include "ZonaRiego.h"
+#include <Arduino.h>
+
+ZonaRiego::ZonaRiego(int id, int sensorPin, int riegoPin, int minH, int maxH) {
+    idZona = id;
+    pinSensor = sensorPin;
+    pinRiego = riegoPin;
+    humedadMin = minH;
+    humedadMax = maxH;
+    estadoRiego = false;
+    pinMode(pinRiego, OUTPUT);
+}
+
+int ZonaRiego::leerHumedad() {
+    return analogRead(pinSensor); // Simulación básica
+}
+
+void ZonaRiego::actualizarHumedad() {
+    int humedad = leerHumedad();
+    if (humedad < humedadMin) {
+        activarRiego();
+    } else if (humedad > humedadMax) {
+        desactivarRiego();
+    }
+}
+
+bool ZonaRiego::debeRegar() {
+    int humedad = leerHumedad();
+    return humedad < humedadMin;
+}
+
+void ZonaRiego::activarRiego() {
+    digitalWrite(pinRiego, HIGH);
+    estadoRiego = true;
+}
+
+void ZonaRiego::desactivarRiego() {
+    digitalWrite(pinRiego, LOW);
+    estadoRiego = false;
+}
+
+bool ZonaRiego::getEstadoRiego() {
+    return estadoRiego;
+}
+
